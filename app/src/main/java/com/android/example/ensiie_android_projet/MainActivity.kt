2ef15980e6a6c34ffd8a30ef96c90d101b33b159
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-11 23:50:35 +0100

    Initialisation

---
+++ app/src/main/java/com/android/example/ensiie_android_projet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.example.ensiie_android_projet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
