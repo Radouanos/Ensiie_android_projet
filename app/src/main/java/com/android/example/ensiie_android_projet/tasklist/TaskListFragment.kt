4f1aa9491b42747116da62b5da277dc96694c4d6
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-12 18:44:53 +0100

    Ajout de tâche simple

--- app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
@@ -8,11 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.example.ensiie_android_projet.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class TaskListFragment : Fragment()
 {
     //private val taskList = listOf("Task 1", "Task 2", "Task 3")
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
             Task(id = "id_1", title = "Task 1", description = "description 1"),
             Task(id = "id_2", title = "Task 2"),
             Task(id = "id_3", title = "Task 3")
@@ -26,7 +28,13 @@ class TaskListFragment : Fragment()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val fab = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+
+        fab.setOnClickListener{
+            taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
+            (recyclerView.adapter)?.notifyDataSetChanged();
+        }
     }
 }
