1f5f4181a43dfdadccb294bb632b5954338ba27e
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-12 16:21:03 +0100

    liste des tâches

---
+++ app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListAdapter.kt
@@ -0,0 +1,27 @@
+package com.android.example.ensiie_android_projet.tasklist
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskListAdapter (private val teskList : List<String>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListAdapter.TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskListAdapter.TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: String) {
+            itemView.apply { // `apply {}` permet d'éviter de répéter `itemView.*`
+                // TODO: afficher les données et attacher les listeners aux différentes vues de notre [itemView]
+            }
+        }
+    }
+}
--- app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.example.ensiie_android_projet.R
 
 class TaskListFragment : Fragment()
 {
+    private val taskList = listOf("Task 1", "Task 2", "Task 3")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //return super.onCreateView(inflater, container, savedInstanceState)
         val rootView = inflater.inflate(R.layout.fragment_task_list,container,false)
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
     }
 }
