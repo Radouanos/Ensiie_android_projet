36c1020bb9c10ce88cfa80c67d654a8a2c1d7903
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-14 11:32:53 +0100

    Suppression d'une tache

--- app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/android/example/ensiie_android_projet/tasklist/TaskListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -38,6 +39,11 @@ class TaskListFragment : Fragment()
         fab.setOnClickListener{
             taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
             adapter.notifyDataSetChanged()
+        }
+        adapter.onDeleteTask = {
+            task ->
+                taskList.remove(task)
+                adapter.notifyDataSetChanged()
         }
     }
 }
