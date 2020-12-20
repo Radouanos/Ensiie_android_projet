8da67d8d31c4df7521e770b03e8bca849541cd59
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-20 22:54:17 +0100

    ajouter d'autres requêtes à l'interface TasksWebService

--- app/src/main/java/com/android/example/ensiie_android_projet/network/TasksWebService.kt
+++ app/src/main/java/com/android/example/ensiie_android_projet/network/TasksWebService.kt
@@ -2,9 +2,17 @@ package com.android.example.ensiie_android_projet.network
 
 import com.android.example.ensiie_android_projet.tasklist.Task
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface TasksWebService {
     @GET("tasks")
     suspend fun getTasks(): Response<List<Task>>
+    @DELETE("tasks/{id}")
+    suspend fun deleteTask(@Path("id") id: String?): Response<Unit>
+
+    @POST("tasks")
+    suspend fun createTask(@Body task: Task): Response<Task>
+
+    @PATCH("tasks/{id}")
+    suspend fun updateTask(@Body task: Task, @Path("id") id: String? = task.id): Response<Task>
 }
