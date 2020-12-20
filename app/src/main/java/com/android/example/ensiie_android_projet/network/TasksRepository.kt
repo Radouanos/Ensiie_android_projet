476e502515adbf2ad1c10036e55dd9b3343f749a
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-20 22:38:31 +0100

    Ajouter le TasksRepository

---
+++ app/src/main/java/com/android/example/ensiie_android_projet/network/TasksRepository.kt
@@ -0,0 +1,27 @@
+package com.android.example.ensiie_android_projet.network
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.example.ensiie_android_projet.tasklist.Task
+
+class TasksRepository {
+    private val tasksWebService = Api.tasksWebService
+
+    // Ces deux variables encapsulent la même donnée:
+    // [_taskList] est modifiable mais privée donc inaccessible à l'extérieur de cette classe
+    private val _taskList = MutableLiveData<List<Task>>()
+    // [taskList] est publique mais non-modifiable:
+    // On pourra seulement l'observer (s'y abonner) depuis d'autres classes
+    public val taskList: LiveData<List<Task>> = _taskList
+
+    suspend fun refresh() {
+        // Call HTTP (opération longue):
+        val tasksResponse = tasksWebService.getTasks()
+        // À la ligne suivante, on a reçu la réponse de l'API:
+        if (tasksResponse.isSuccessful) {
+            val fetchedTasks = tasksResponse.body()
+            // on modifie la valeur encaapsulée, ce qui va notifier ses Observers et donc déclencher leur callback
+            _taskList.value = fetchedTasks!!
+        }
+    }
+}
