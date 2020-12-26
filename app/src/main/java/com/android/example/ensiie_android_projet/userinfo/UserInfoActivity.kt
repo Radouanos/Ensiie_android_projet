4e6b9a9de16575e851d4b5454f9b25ca1cad5661
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-26 21:00:09 +0100

    Accéder aux fichiers locaux & Uploader une image stockée

--- app/src/main/java/com/android/example/ensiie_android_projet/userinfo/UserInfoActivity.kt
+++ app/src/main/java/com/android/example/ensiie_android_projet/userinfo/UserInfoActivity.kt
@@ -8,14 +8,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import coil.load
+import com.android.example.ensiie_android_projet.BuildConfig
 import com.android.example.ensiie_android_projet.R
 import com.android.example.ensiie_android_projet.network.Api
 import com.android.example.ensiie_android_projet.network.TasksWebService
@@ -26,6 +29,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 
 class UserInfoActivity : AppCompatActivity() {
+
     lateinit var avatar:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,8 +37,14 @@ class UserInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_info)
         avatar = findViewById(R.id.avatar2)
         val takePic = findViewById<Button>(R.id.take_picture_button)
+        val uploadIm = findViewById<Button>(R.id.upload_image_button)
+
         takePic.setOnClickListener {
             askCameraPermissionAndOpenCamera()
+        }
+
+        uploadIm.setOnClickListener{
+            getFromGallery()
         }
 
     }
@@ -68,18 +78,6 @@ class UserInfoActivity : AppCompatActivity() {
             else showExplanationDialog()
         }
 
-    // register
-    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
-        val tmpFile = File.createTempFile("avatar", "jpeg")
-        tmpFile.outputStream().use {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
-        }
-        handleImage(tmpFile.toUri())
-    }
-
-    // use
-    private fun openCamera() = takePicture.launch()
-
     private fun handleImage(toUri: Uri)
     {
         val converted = convert(toUri)
@@ -103,4 +101,28 @@ class UserInfoActivity : AppCompatActivity() {
             avatar.load(userInfo.body()?.avatar)
         }
     }
+    // create a temp file and get a uri for it
+    private val photoUri by lazy {
+        FileProvider.getUriForFile(
+                this,
+                BuildConfig.APPLICATION_ID +".fileprovider",
+                File.createTempFile("avatar", ".jpeg", externalCacheDir)
+
+        )
+    }
+
+    // register
+    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+        if (success) handleImage(photoUri)
+        else
+            Toast.makeText(this, "Erreur ! 😢", Toast.LENGTH_LONG).show()
+    }
+
+    private val pickInGallery = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        handleImage(uri)
+    }
+
+    private fun getFromGallery() = pickInGallery.launch("image/*")
+    // use
+    private fun openCamera() = takePicture.launch(photoUri)
 }
